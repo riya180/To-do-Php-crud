6b61d1922bd68e6fa265efe5b70f6c9e1c2a98e9
Riya <riyathakurprof@gmail.com>
2024-03-05 15:26:05 +0530

    first commit

---
+++ form.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>login form</title>
+</head>
+<body>
+	
+	<div class="container">
+		<div class="title"> Login Form</div>
+
+		<div class="form">
+			<div class="input-field">
+				<label>First Name</label>
+				<input type="text" class="input">
+			</div>
+			<div class="input-field">
+				<label>Last Name</label>
+				<input type="text" class="input">
+			</div>
+			<div class="input-field">
+				<label>Email</label>
+				<input type="text" class="input">
+			</div>
+			<div class="input-field">
+				<label>Password</label>
+				<input type="Password" class="input">
+			</div>
+			<div class="input-field">
+				<label>Confirm Password</label>
+				<input type="Password" class="input">
+			</div>
+			<div class="input-field">
+				<label>Gendder</label>
+				<select>
+					<option>select</option>
+					<option>Male</option>
+					<option>Female</option>
+					<option>Others</option>
+				</select>
+			</div>
+			<div class="input-field">
+				<label>Phone</label>
+				<input type="text" class="input">
+			</div>
+			<div class="input-field">
+				<label>Address</label>
+				<textarea> </textarea>
+			</div>
+			<div class="input-field-term">
+				<label class="check">
+					<input type="checkbox">
+					<span class="checkmark">
+						<p>Agree terms and conditions</p>
+					</span>
+				</label>
+			</div>
+			<div class="input-field">
+				<input type="submit" value="register" class="btn">
+			</div>
+			
+		</div>
+	</div>
+
+
+</body>
+</html>
